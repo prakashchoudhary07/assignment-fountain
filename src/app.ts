f96f7061a8f2568ee0880e44c684af91c421dcbf
Prakash <om.pcprakash@gmail.com>
2024-05-06 16:48:09 +0530

    feat: add global error handler

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,16 @@ import config from 'config';
 
 import baseRoutes from './routes/index';
 import appMiddleware from './middleware/index';
+import { errorHandler } from './utils/errorhandler';
 
 const app = express();
 const port = config.get('PORT') ?? 3002;
 
 appMiddleware(app);
 app.use('/api/v1', baseRoutes);
+app.use(errorHandler);
 
 app.listen(port, () => {
+  // TODO: use logger
   console.log(`Server running at http://localhost:${port}`);
 });
